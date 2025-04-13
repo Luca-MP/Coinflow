8e1e8869db26c0c6dcaf610af4b5489839fd33e3
Luca Michael Pezzotta <lucamichael.pezzotta@sisal.it>
2025-04-13 18:59:35 +0200

    Add viewmodel tests, update error message

--- app/src/main/java/it/pezzotta/coinflow/common/ErrorMessage.kt
+++ app/src/main/java/it/pezzotta/coinflow/common/ErrorMessage.kt
@@ -3,15 +3,26 @@ package it.pezzotta.coinflow.common
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.ui.res.stringResource
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import it.pezzotta.coinflow.R
 import it.pezzotta.coinflow.data.model.Coin
@@ -30,28 +41,29 @@ fun ErrorMessage(
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Icon(
+            imageVector = Icons.Rounded.Warning,
+            contentDescription = "Warning",
+            Modifier.size(80.dp),
+            tint = MaterialTheme.colorScheme.error
+        )
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
-                text = stringResource(R.string.warning_sign),
-                fontSize = 32.sp,
-                textAlign = TextAlign.Center,
-            )
-            Text(
                 text = stringResource(R.string.ops),
-                color = MaterialTheme.colorScheme.error,
                 style = MaterialTheme.typography.titleLarge,
                 textAlign = TextAlign.Center,
             )
-        }
-        Button(
-            onClick = {
-                if (!isDetailsScreen) coinViewModel?.getCoinMarket() else coinViewModel?.getCoinDetails(
-                    coin,
-                    days,
-                    precision,
-                )
-            }) {
-            Text(text = stringResource(R.string.retry))
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 0.5f).padding(top = 24.dp),
+                onClick = {
+                    if (!isDetailsScreen) coinViewModel?.getCoinMarket() else coinViewModel?.getCoinDetails(
+                        coin,
+                        days,
+                        precision,
+                    )
+                }) {
+                Text(text = stringResource(R.string.retry))
+            }
         }
     }
 }
---
+++ app/src/test/java/it/pezzotta/coinflow/viewmodel/CoinViewModelTest.kt
@@ -0,0 +1,133 @@
+import it.pezzotta.coinflow.data.model.Coin
+import it.pezzotta.coinflow.data.model.CoinData
+import it.pezzotta.coinflow.data.model.CoinDetails
+import it.pezzotta.coinflow.data.model.CoinMarketHistory
+import it.pezzotta.coinflow.data.repository.CoinRepository
+import it.pezzotta.coinflow.viewmodel.CoinViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlinx.serialization.json.Json
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+@ExperimentalCoroutinesApi
+class CoinViewModelTest {
+    private val testDispatcher = StandardTestDispatcher()
+    private val testScope = TestScope(testDispatcher)
+    private val json = Json { ignoreUnknownKeys = true }
+
+    private lateinit var coinRepository: CoinRepository
+    private lateinit var coinViewModel: CoinViewModel
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        coinRepository = mock()
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `getCoinMarket should update coinMarket state with success result`() = testScope.runTest {
+        val market = javaClass.classLoader?.getResourceAsStream("market_response.json")
+            ?: throw AssertionError("Resource not found: market_response.json")
+        val marketJson = market.bufferedReader().use { it.readText() }
+        val marketData = json.decodeFromString<List<Coin>>(marketJson)
+        val marketResult = Result.success(marketData)
+
+        doReturn(marketResult).`when`(coinRepository).getCoinMarket()
+        coinViewModel = CoinViewModel(coinRepository)
+
+        advanceUntilIdle()
+        val actualResult = coinViewModel.coinMarket.value?.getOrNull()
+
+        assertEquals(marketData, actualResult)
+
+        verify(coinRepository).getCoinMarket()
+    }
+
+    @Test
+    fun `getCoinMarket should update coinMarket state with failure result`() = testScope.runTest {
+        val exception = Exception("Error 429:")
+        val marketResult = Result.failure<List<Coin>>(exception)
+
+        doReturn(marketResult).`when`(coinRepository).getCoinMarket()
+        coinViewModel = CoinViewModel(coinRepository)
+
+        advanceUntilIdle()
+        val actualResult = coinViewModel.coinMarket.value
+
+        assertTrue(actualResult?.isFailure == true)
+        assertEquals("Error 429:", actualResult?.exceptionOrNull()?.message)
+
+        verify(coinRepository).getCoinMarket()
+    }
+
+    @Test
+    fun `getCoinDetails should update coinDetails state with success result`() = testScope.runTest {
+        val market = javaClass.classLoader?.getResourceAsStream("market_response.json")
+            ?: throw AssertionError("Resource not found: market_response.json")
+        val marketJson = market.bufferedReader().use { it.readText() }
+        val marketData = Json.decodeFromString<List<Coin>>(marketJson)
+
+        val coinData = javaClass.classLoader?.getResourceAsStream("coin_data_response.json")
+            ?: throw AssertionError("Resource not found: coin_data_response.json")
+        val coinDataJson = coinData.bufferedReader().use { it.readText() }
+        val coinDataData = json.decodeFromString<CoinData>(coinDataJson)
+
+        val coinMarketHistory = javaClass.classLoader?.getResourceAsStream("coin_market_history_response.json")
+            ?: throw AssertionError("Resource not found: coin_market_history_response.json")
+        val coinMarketHistoryJson = coinMarketHistory.bufferedReader().use { it.readText() }
+        val coinMarketHistoryData = json.decodeFromString<CoinMarketHistory>(coinMarketHistoryJson)
+
+        val coinDetailsResult = Result.success(CoinDetails(coinDataData, coinMarketHistoryData))
+        val days = 7
+        val precision = 8
+
+        doReturn(coinDetailsResult).`when`(coinRepository).getCoinDetails(marketData.first(), days, precision)
+        coinViewModel = CoinViewModel(coinRepository)
+        coinViewModel.getCoinDetails(marketData.first(), days, precision)
+
+        advanceUntilIdle()
+        val actualResult = coinViewModel.coinDetails.value?.getOrNull()
+
+        assertEquals(coinDetailsResult.getOrNull(), actualResult)
+
+        verify(coinRepository).getCoinDetails(marketData.first(), days, precision)
+    }
+
+    @Test
+    fun `getCoinDetails should update coinDetails state with failure result`() = testScope.runTest {
+        val exception = Exception("Error 429:")
+        val coinDetailsResult = Result.failure<CoinDetails>(exception)
+        val days = 7
+        val precision = 2
+
+        doReturn(coinDetailsResult).`when`(coinRepository).getCoinDetails(Coin(), days, precision)
+        coinViewModel = CoinViewModel(coinRepository)
+        coinViewModel.getCoinDetails(Coin(), days, precision)
+
+        advanceUntilIdle()
+        val actualResult = coinViewModel.coinDetails.value
+
+        assertTrue(actualResult?.isFailure == true)
+        assertEquals("Error 429:", actualResult?.exceptionOrNull()?.message)
+
+        verify(coinRepository).getCoinDetails(Coin(), days, precision)
+    }
+}
